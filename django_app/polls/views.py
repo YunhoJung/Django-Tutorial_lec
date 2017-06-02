53dd5a91e7315d906d0e3cf9941ac3076b5dd5c0
Jung YunHo <saijdu2198@gmail.com>
2017-06-02 12:06:04 -0700

    add polls/detail.html, use template in detail view

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Question
@@ -7,17 +7,36 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
+    # template = loader.get_template('polls/index.html') + HttpResponse 이 과정을 줄인 게 render 함수
     context = {
-        'latest_question_list':latest_question_list,
+        'latest_question_list': latest_question_list,
     }
     return render(request, 'polls/index.html', context)
+
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # question_id가 pk인 Question객체를 가져와
+    # context라는 이름을 가진 dict에 'question'이라는 키 값으로 위 변수를 전달
+    # 이후 'polls/detail.html'과 context를 렌더한 결과를 리턴
+
+    # polls/detail에서 해당 question의 question_text를 출력
+    try:
+        question = Question.objects.get(pk=question_id) # get(쿼리셋에서 어떤 것을 집어오는 것)함수는 pk값을 받는다 !
+    except Question.DoesNotExist as e:
+        raise Http404('Question does not exist')
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/detail.html', context)
+
 
 def results(request, question_id):
+    # question_id가 pk인 Question객체를 가져와
+    # context라는 이름을 가진 dict에 'question'이라는 키 값으로 위 변수를 전달
+    # 이후 'polls/detail.html'과 context를 렌더한 결과를 리턴
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
