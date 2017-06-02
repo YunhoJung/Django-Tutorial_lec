389258ba67932c82601918df77660f17ff9ade47
Jung YunHo <saijdu2198@gmail.com>
2017-06-02 16:45:00 -0700

    modify admin page

--- django_app/polls/models.py
+++ django_app/polls/models.py
@@ -13,6 +13,9 @@ class Question(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.short_description = '최근발행여부'
+    was_published_recently.boolean = True
+    # was_published_recently.admin_order_field =
 
 
 class Choice(models.Model):
